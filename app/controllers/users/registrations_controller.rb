88d7188ea4e0df08675fdc33a03b58113678480a
Mathieu Lague <mathieu@lague.me>
2024-06-24 12:29:46 +0200

    add usernames network fields

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -42,16 +42,17 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:photo, :username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:photo, :username, network_usernames: {}])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:photo, :username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:photo, :username, network_usernames: {}])
   end
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
+
     super(resource)
   end
 
