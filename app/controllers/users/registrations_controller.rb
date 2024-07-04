e5a755dcbd12aac71e1b682e6698dd987c64625c
Mathieu Lague <mathieu@lague.me>
2024-07-04 12:10:23 +0200

    some details on profile & registretion controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -42,12 +42,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:photo, :username, network_usernames: {}])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:photo, :username, :bio, network_usernames: {}])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:photo, :username, network_usernames: {}])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:photo, :username, :bio, network_usernames: {}])
   end
 
   # The path used after sign up.
