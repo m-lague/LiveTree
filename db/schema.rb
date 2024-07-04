77625cb1cd65d3f47f23bbdff95bc91c3aba257f
Mathieu Lague <mathieu@lague.me>
2024-07-04 14:54:27 +0200

    create Tag

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.1].define(version: 2024_06_12_062614) do
-
+ActiveRecord::Schema[7.1].define(version: 2024_07_04_125258) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,6 +50,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_12_062614) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_lives_on_user_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
